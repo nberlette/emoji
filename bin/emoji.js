d7f94ab3b367e61204d9b04e649364ae46d17864
Nicholas Berlette <nick@berlette.com>
2021-11-10 11:56:29 -0800

    updates and bugfix for es import

--- bin/emoji.js
+++ bin/emoji.js
@@ -6,10 +6,9 @@ const { argv } = process
 
 // gross, I know
 argv[0].endsWith('node') && argv.shift()
-const q = argv[1] ?? ''
+const q = argv[1] || ''
 
-let emoji = require('../')
-emoji = new Map([...Object.entries(emoji)])
+const emoji = new Map([...Object.entries(require('@nberlette/emoji'))])
 
 function filter (a, q) {
   return a.filter(el => el.toLowerCase().indexOf(q.toLowerCase()) !== -1)
@@ -19,8 +18,9 @@ function getEmoji (q) {
   // direct match? return just the emoji
   if (emoji.has(q)) return log(emoji.get(q))
 
-  // otherwise find closest matches (no limit implemented yet)
-  const results = filter([...emoji.keys()], q).map(k => [`${(emoji.has(k) ? emoji.get(k) : null)} `, k])
+  const results = filter([...emoji.keys()], q)
+    .map(k => [`${(emoji.has(k) ? emoji.get(k) : null)} `, k])
+    .slice(0, 50)
 
   if (results.length > 0) results.forEach(r => (r !== undefined) && log(r.join('  ')))
   else log('no results')
